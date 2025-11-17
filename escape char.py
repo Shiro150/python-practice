dabbfb9c0b30bbccb92a8f967cebcf1edf50c7c4
Shiro <64950860+Shiro150@users.noreply.github.com>
2025-11-17 22:15:15 +0530

    escapechar

---
+++ escape char.py
@@ -0,0 +1,10 @@
+splitstring = 'the string has been \nsplit \n multiple \n times'
+print(splitstring)
+tabbedString = ('1\t2\t3\t4')
+print(tabbedString)
+print("the pet store owner said \" no , ""no ,,, he's resting")
+print('''the pet store owner said no no ... he's''')
+anotherssplit = '''the string 
+has been \
+split in multiple lines'''
+print(anotherssplit)

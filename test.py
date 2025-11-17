865736ec0dda77e9190e403c20efdcaf130393ff
Shiro <64950860+Shiro150@users.noreply.github.com>
2025-11-18 02:29:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,38 @@ name = 'animesh'
 
 print(type(age))
 print(type(name))
+print(16//5)
+print(15%4)
+
+text = "elden ring is game"
+
+print(text[5:-1]) #slicer
+print(text[0:10:3])  #eerg
+
+letters = "abcdefghijklmnopqrstuwvxyz"
+
+backwards = letters[16:13:-1]
+#backwards = letters[5::-1]
+print(backwards)
+
+print('hello' * (5+4))
+
+print("elden ring" in text)  #checks left with right and gives true and false
+
+age = 24
+str(age)
+print(type(age))
+
+#format
+print("my age is {0}".format(230))
+
+print("the ans is {0:12}".format(22/7))
+print("the ans is {0:50f}".format(22/7))
+print("the ans is {0:12.50f}".format(22/7))
+print("the ans is {0:>50.25f}".format(22/7))
+
+#fstring
+print(f"my age is {age}")
+pi=22/7
+print(f"the ans is {22/7:15.10f}")
+

5905b75e2a359cf62a5d5213d748049e9e00e333
Shiro <64950860+Shiro150@users.noreply.github.com>
2025-11-17 21:46:56 +0530

    strings concat

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,3 @@
 print('Hello, World!!')
+print(7+8, 7*6)
+print('learning pythom')
---
+++ strings.py
@@ -0,0 +1,7 @@
+print('today is a good day to learn python')
+print('python is fun')
+print('hello'+'world')
+firstname = 'animesh'
+lastname = 'parab'
+middlename = input('please enter your middle name =')
+print(firstname+' '+middlename+' ' +lastname)

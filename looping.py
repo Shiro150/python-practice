c2b44a12293319a14a2060f31394874578f387c7
Shiro <64950860+Shiro150@users.noreply.github.com>
2025-11-18 22:54:30 +0530

    looping

---
+++ looping.py
@@ -0,0 +1,31 @@
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+#
+# for numbers in numbers:
+#     if numbers ==5:
+#         print("found!!")
+#         print(numbers)
+#         break #break/close the loop     continue to keep the loop going
+#     else:
+#         print(numbers)
+#
+#
+# for i in range(1, 11):
+#     print(i)
+#
+#
+# x = 0
+# while x<10:
+#     print(x)
+#     x += 1
+
+# arr = ('a','b','c','d','e','f','g','h','i','j','k','l')
+#
+# for i in range(0,len(arr),2):
+#     print(arr[i])
+
+# value = 8
+# answer = 0
+#
+# for x in range(1, 13):
+#     answer = value * x
+#     print("{0} times {1} is {2}".format(x, value, answer))

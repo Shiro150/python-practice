d818e2cf6ec0fbb400610407c819aa8a2a4e9aad
Shiro <64950860+Shiro150@users.noreply.github.com>
2025-11-18 20:26:51 +0530

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,48 @@
+from enum import nonmember
+from operator import truediv
+from selectors import SelectSelector
+
+# language = 'ja'
+#
+# if language == 'python':
+#     print('the language is python')
+# elif language =='java':
+#     print('language is java')
+# else:
+#     print("no match")
+
+#and
+#or
+#not
+
+# user = 'admin'
+# logged_in = False
+# user_ID = 123
+#
+# if user=='admin' and user_ID== 123 and not logged_in:
+#     print('admin page')
+# else:
+#     print('not match')
+#
+# a = [1,2,3]
+# # b = [1,2,3]
+# #
+# # print(id(a))
+# # print(id(b))
+# # print(a == b)
+# # print(a is b)
+
+# # false values
+# # False
+# # none
+# # zero of any number type
+# # any empty (){}[]
+#
+# condition = 0
+# if condition:
+#     print('true')
+# else:
+#     print('false')
+#
+# print(condition)
+
